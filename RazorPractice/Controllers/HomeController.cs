5d78808219658a9789c8fd01992880922642fce2
Michal Andrejszyn <m.andrejszyn@luca.eu>
2019-05-19 10:49:10 +0200

    Base project structure added

---
+++ RazorPractice/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Category = "Water Sports",
+                Description = "One man bolt",
+                Price = 275M
+            };
+            return View(myProduct);
+        }
+    }
+}
